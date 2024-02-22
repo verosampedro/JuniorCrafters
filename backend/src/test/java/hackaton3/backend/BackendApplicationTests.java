dd12abf3196e62081aa07e6d76386ccff23b2036
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-02-22 09:11:08 +0100

    Creado Backend

---
+++ backend/src/test/java/hackaton3/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hackaton3.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

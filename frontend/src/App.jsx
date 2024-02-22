e1a3466341df5de9fec107c7b774b5d438a2351b
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-02-22 20:35:20 +0100

    commit from merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import ContactForm from './pages/ContactForm/ContactForm';
-import Description from './components/atoms/Description';
+import Description from './pages/Description/Description';
+import Header from "./components/organisms/Header/Header";
+import { CartProvider } from "./CartContext";
+import EasterEgg from "./pages/EasterEgg/FlipatedVerse";
+// import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <>
       <Router>
+        <CartProvider>
+        <Header />
         <Routes>
-          <Route path="/contact" element={<ContactForm />} />
-          <Route path="/description" element={<Description />} />
+          {/* <Route path="/" element={<Home />}/> */}
+          <Route path="/contact" element={<ContactForm />}/>
+          <Route path="/description" element={<Description />}/>
+          <Route path="/multiverse" element={<EasterEgg />} />
         </Routes>
+      </CartProvider>
       </Router>
     </>
   )

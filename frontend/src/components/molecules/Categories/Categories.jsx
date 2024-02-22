e6bdd4eef758c99bd9b1948b73c32d0432efe8e1
EstefanyBatPel <estefanybatpel@gmail.com>
2024-02-22 17:44:35 +0100

    fetch

--- frontend/src/components/molecules/Categories/Categories.jsx
+++ frontend/src/components/molecules/Categories/Categories.jsx
@@ -1,7 +1,58 @@
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
 import React from 'react';
 import './Categories.css';
 
 const Categories = () => {
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/course");
+        console.log("Respuesta del servidor:", response.data);
+        const dataApi = response.data;
+        const filteredImages = dataApi.data.filter((image) => {
+          const hasCategory = !filter || image.keyword.includes(filter);
+          const matchesSearch =
+            !searchTerm ||
+            image.title.toLowerCase().includes(searchTerm.toLowerCase());
+          return hasCategory && matchesSearch;
+        });
+
+        setSearchTerm(searchTerm);
+        setGalleryImages(filteredImages);
+        console.log("Número de imágenes después del filtrado:", filteredImages.length);
+        setImages(filteredImages);
+      } catch (error) {
+        console.error("Error al obtener los datos de la API:", error);
+      }
+    };
+
+    fetchData();
+    const galleryContainer = galleryContainerRef.current;
+
+    const updateContainerRect = () => {
+      if (galleryContainer) {
+        setContainerRect(galleryContainer.getBoundingClientRect());
+      }
+    };
+
+    updateContainerRect();
+
+    const handleResize = () => {
+      updateContainerRect();
+    };
+
+    galleryContainer.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      galleryContainer.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [category, filter, searchTerm]);
+
   return (
     <div className='containerCategories'>
             <div className="category1"></div>

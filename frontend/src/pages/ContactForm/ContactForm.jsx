f3608414a0deef67f3476c4108891429d4edcb72
albamartinmz <albamartinsonido@gmail.com>
2024-02-22 13:24:11 +0100

    edit contact styles

--- frontend/src/pages/ContactForm/ContactForm.jsx
+++ frontend/src/pages/ContactForm/ContactForm.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ContactForm.css'
 import { useForm } from "react-hook-form"
+import contact from  "../../assets/images/contact_bg.png";
 
 
 const ContactForm = () => {
@@ -12,26 +13,29 @@ const ContactForm = () => {
     };
 
     return (
-        <main className="form-container">
-            <div className="contact-form">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <h2>CONTACT US</h2>
-                    <div>
-                        <label>Name</label>
-                        <input type="text" {...register('name', { required: true, maxLength:  40 })} />
-                    </div>
-                    <div>
-                        <label>Email</label>
-                        <input type="text" {...register('email', { required: true })} />
-                    </div>
-                    <div>
-                        <label>Message</label>
-                        <input type="text-area" {...register('message', { required: true })} />
-                    </div>
-                    <input className="contact-button" type="submit" value="SEND" />
-                </form>
-            </div>
-        </main>
+        <section className="box-contact">
+            <main className="form-container">
+                <div className="contact-form">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <h2>CONTACT US</h2>
+                        <div>
+                            <label>Name</label>
+                            <input type="text" {...register('name', { required: true, maxLength:  40 })} />
+                        </div>
+                        <div>
+                            <label>Email</label>
+                            <input type="text" {...register('email', { required: true })} />
+                        </div>
+                        <div>
+                            <label>Message</label>
+                            <input className="message-input" type="text" {...register('message', { required: true })} />
+                        </div>
+                        <input className="contact-button" type="submit" value="SEND" />
+                    </form>
+                </div>
+            </main>
+            <img src={contact} alt="" className="contact-image"/>
+        </section>
 )
 }
 
